49add3221ecee3ec0d3854bce5d0d7531358d11a
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2026-02-06 08:28:55 +0100

    wip

--- src/clojure/server.clj
+++ src/clojure/server.clj
@@ -63,6 +63,39 @@
                   (throw (UnsupportedOperationException. "Unsupported OS")))]
     builder))
 
+(pr/defmapper internal-mapper [Schema$Attribute])
+
+;; "string"
+;; :string
+;; "number"
+;; :number
+;; "bool"
+;; :bool
+;; ["map","string"]
+;; [:map :string]
+;; ["set","string"]
+;; [:set :string]
+;; ["list","string"]
+;; [:list :string]
+;; ["list",["object",{"type":"string"}]]
+;; [:list [object {:type :string}]]
+;; ["list",["object",{"delete_protection":"bool","description":"string","home_location":"string","id":"number","ip_address":"string","ip_network":"string","labels":["map","string"],"name":"string","server_id":"number","type":"string"}]]
+;; [:list [:object {:delete_protection :bool :description :string :labels [:map :string]}]]
+;; ["list",["object",{"algorithm":["list",["object",{"type":"string"}]],"delete_protection":"bool","id":"number","ipv4":"string","ipv6":"string","labels":["map","string"],"load_balancer_type":"string","location":"string","name":"string","network_id":"number","network_ip":"string","network_zone":"string","service":["list",["object",{"destination_port":"number","health_check":["list",["object",{"http":["list",["object",{"domain":"string","path":"string","response":"string","status_codes":["list","number"],"tls":"bool"}]],"interval":"number","port":"number","protocol":"string","retries":"number","timeout":"number"}]],"http":["list",["object",{"certificates":["list","string"],"cookie_lifetime":"number","cookie_name":"string","redirect_http":"bool","sticky_sessions":"bool"}]],"listen_port":"number","protocol":"string","proxyprotocol":"bool"}]],"target":["list",["object",{"label_selector":"string","server_id":"number","type":"string"}]]}]]
+;; [:list [:object {:algorithm [:list [:object {:type :string}]] :delete_protection :bool}]]
+
+(defn byte-string->tf-type [^ByteString tf-type]
+  (-> tf-type
+      .toByteArray
+      io/reader
+      (json/parse-stream true)
+      (->> (s/transform (s/walker string?) keyword))))
+
+(defn tf-type->byte-string [tf-type]
+  (-> tf-type
+      json/generate-string
+      ByteString/copyFromUtf8))
+
 (pr/defmapper provider-mapper [GetProviderSchema$Request
                                GetProviderSchema$Response
                                ValidateResourceConfig$Request
@@ -73,7 +106,27 @@
                                ConfigureProvider$Response
                                PlanResourceChange$Request
                                PlanResourceChange$Response
-                               ServerCapabilities])
+                               ServerCapabilities]
+  :encoders {Schema$Attribute
+             {:from-proto (fn [attribute]
+                            (->> (into {} (pr/proto->proto-map internal-mapper attribute))
+                                 (s/transform [:type] byte-string->tf-type)))
+              :to-proto (fn [attribute]
+                          (->> attribute
+                               (s/transform [:type] tf-type->byte-string)
+                               (pr/clj-map->proto-map internal-mapper Schema$Attribute)
+                               (pr/proto-map->proto)))}
+
+             DynamicValue
+             {:from-proto (fn [^DynamicValue dynamic-value]
+                            {:msgpack (-> (.getMsgpack dynamic-value)
+                                          .toByteArray
+                                          unpack)
+                             :json (-> (.getJson dynamic-value)
+                                       .toByteArray
+                                       io/reader
+                                       (json/parse-stream true))})
+              :to-proto identity}})
 
 (defn- ->provider-service []
   (proxy [ProviderGrpc$ProviderImplBase] []
@@ -352,51 +405,6 @@
 
 (comment
   (do
-    (pr/defmapper internal-mapper [Schema$Attribute])
-
-    (defn byte-string->tf-type [^ByteString tf-type]
-      (-> tf-type
-          .toByteArray
-          io/reader
-          (json/parse-stream true)
-          (->> (s/transform (s/walker string?) keyword))))
-
-    (defn tf-type->byte-string [tf-type]
-      (-> tf-type
-          json/generate-string
-          ByteString/copyFromUtf8))
-
-    (pr/defmapper provider-mapper [GetProviderSchema$Request
-                                   GetProviderSchema$Response
-                                   ValidateResourceConfig$Request
-                                   ValidateResourceConfig$Response
-                                   ValidateProviderConfig$Request
-                                   ValidateProviderConfig$Response
-                                   ConfigureProvider$Request
-                                   ConfigureProvider$Response
-                                   PlanResourceChange$Request
-                                   PlanResourceChange$Response
-                                   ServerCapabilities]
-      :encoders {Schema$Attribute
-                 {:from-proto (fn [attribute]
-                                (->> (into {} (pr/proto->proto-map internal-mapper attribute))
-                                     (s/transform [:type] byte-string->tf-type)))
-                  :to-proto (fn [attribute]
-                              (->> attribute
-                                   (s/transform [:type] tf-type->byte-string)
-                                   (pr/clj-map->proto-map internal-mapper Schema$Attribute)
-                                   (pr/proto-map->proto)))}
-
-                 DynamicValue
-                 {:from-proto (fn [^DynamicValue dynamic-value]
-                                {:msgpack (-> (.getMsgpack dynamic-value)
-                                              .toByteArray
-                                              unpack)
-                                 :json (-> (.getJson dynamic-value)
-                                           .toByteArray
-                                           io/reader
-                                           (json/parse-stream true))})
-                  :to-proto identity}})
     (defn process [v]
       (binding [*out* *err*]
         #_(println (class v))
@@ -418,40 +426,8 @@
   (into (sorted-map) (main-wf {::block true
                                ::bc/env :repl})))
 
-;; "string"
-;; :string
-;; "number"
-;; :number
-;; "bool"
-;; :bool
-;; ["map","string"]
-;; [:map :string]
-;; ["set","string"]
-;; [:set :string]
-;; ["list","string"]
-;; [:list :string]
-;; ["list",["object",{"type":"string"}]]
-;; [:list [object {:type :string}]]
-;; ["list",["object",{"delete_protection":"bool","description":"string","home_location":"string","id":"number","ip_address":"string","ip_network":"string","labels":["map","string"],"name":"string","server_id":"number","type":"string"}]]
-;; [:list [:object {:delete_protection :bool :description :string :labels [:map :string]}]]
-;; ["list",["object",{"algorithm":["list",["object",{"type":"string"}]],"delete_protection":"bool","id":"number","ipv4":"string","ipv6":"string","labels":["map","string"],"load_balancer_type":"string","location":"string","name":"string","network_id":"number","network_ip":"string","network_zone":"string","service":["list",["object",{"destination_port":"number","health_check":["list",["object",{"http":["list",["object",{"domain":"string","path":"string","response":"string","status_codes":["list","number"],"tls":"bool"}]],"interval":"number","port":"number","protocol":"string","retries":"number","timeout":"number"}]],"http":["list",["object",{"certificates":["list","string"],"cookie_lifetime":"number","cookie_name":"string","redirect_http":"bool","sticky_sessions":"bool"}]],"listen_port":"number","protocol":"string","proxyprotocol":"bool"}]],"target":["list",["object",{"label_selector":"string","server_id":"number","type":"string"}]]}]]
-;; [:list [:object {:algorithm [:list [:object {:type :string}]] :delete_protection :bool}]]
-
-(comment
-  (let [xs #{[:list [:object {"iso" :string, "placement_group_id" :number, "firewall_ids" [:list :number], "server_type" :string, "image" :string, "id" :number, "ipv6_address" :string, "backup_window" :string, "backups" :bool, "delete_protection" :bool, "ipv6_network" :string, "primary_disk_size" :number, "rebuild_protection" :bool, "name" :string, "network" [:set [:object {"alias_ips" [:set :string], "ip" :string, "mac_address" :string, "network_id" :number}]], "labels" [:map :string], "location" :string, "status" :string, "ipv4_address" :string, "datacenter" :string, "rescue" :string}]] [:map :string] [:list [:object {"id" :number, "delete_protection" :bool, "server_id" :number, "name" :string, "ip_network" :string, "labels" [:map :string], "type" :string, "ip_address" :string, "description" :string, "home_location" :string}]] :number [:set :string] [:list [:object {"rapid_deploy" :bool, "created" :string, "os_flavor" :string, "architecture" :string, "id" :number, "name" :string, "labels" [:map :string], "deprecated" :string, "type" :string, "selector" :string, "os_version" :string, "description" :string}]] [:list [:object {"apply_to" [:set [:object {"label_selector" :string, "server" :number}]], "id" :number, "labels" [:map :string], "name" :string, "rule" [:set [:object {"description" :string, "destination_ips" [:set :string], "direction" :string, "port" :string, "protocol" :string, "source_ips" [:set :string]}]]}]] [:list [:object {"ipv4" :string, "network_zone" :string, "ipv6" :string, "id" :number, "delete_protection" :bool, "algorithm" [:list [:object {"type" :string}]], "name" :string, "labels" [:map :string], "location" :string, "network_id" :number, "target" [:list [:object {"label_selector" :string, "server_id" :number, "type" :string}]], "service" [:list [:object {"destination_port" :number, "health_check" [:list [:object {"http" [:list [:object {"domain" :string, "path" :string, "response" :string, "status_codes" [:list :number], "tls" :bool}]], "interval" :number, "port" :number, "protocol" :string, "retries" :number, "timeout" :number}]], "http" [:list [:object {"certificates" [:list :string], "cookie_lifetime" :number, "cookie_name" :string, "redirect_http" :bool, "sticky_sessions" :bool}]], "listen_port" :number, "protocol" :string, "proxyprotocol" :bool}]], "load_balancer_type" :string, "network_ip" :string}]] [:set :number] [:list [:object {"id" :number, "labels" [:map :string], "name" :string, "servers" [:list :number], "type" :string}]] [:list [:object {"label_selector" :string, "server_id" :number, "type" :string}]] :string [:list [:object {"assignee_type" :string, "id" :number, "delete_protection" :bool, "name" :string, "ip_network" :string, "labels" [:map :string], "location" :string, "assignee_id" :number, "type" :string, "ip_address" :string, "datacenter" :string, "auto_delete" :bool}]] [:list :number] :nil [:list :string] [:list [:object {"delete_protection" :bool, "expose_routes_to_vswitch" :bool, "id" :number, "ip_range" :string, "labels" [:map :string], "name" :string}]] [:list [:object {"delete_protection" :bool, "id" :number, "labels" [:map :string], "linux_device" :string, "location" :string, "name" :string, "server_id" :number, "size" :number}]] :bool [:list [:object {"domain_names" [:list :string], "created" :string, "fingerprint" :string, "not_valid_before" :string, "id" :number, "certificate" :string, "name" :string, "labels" [:map :string], "type" :string, "not_valid_after" :string}]] [:list [:object {"type" :string}]] [:list [:object {"destination_port" :number, "health_check" [:list [:object {"http" [:list [:object {"domain" :string, "path" :string, "response" :string, "status_codes" [:list :number], "tls" :bool}]], "interval" :number, "port" :number, "protocol" :string, "retries" :number, "timeout" :number}]], "http" [:list [:object {"certificates" [:list :string], "cookie_lifetime" :number, "cookie_name" :string, "redirect_http" :bool, "sticky_sessions" :bool}]], "listen_port" :number, "protocol" :string, "proxyprotocol" :bool}]]}]
-    (-> xs
-        (->> (map #(identity {:provider {:block {:attributes [{:type %}]}}})))
-        first
-        #_(->> (s/transform [:type] tf-type->byte-string))
-        #_(->> (pr/clj-map->proto-map provider-mapper GetProviderSchema$Response)))))
-
-(comment
-  (let [xs #{[:list [:object {"iso" :string, "placement_group_id" :number, "firewall_ids" [:list :number], "server_type" :string, "image" :string, "id" :number, "ipv6_address" :string, "backup_window" :string, "backups" :bool, "delete_protection" :bool, "ipv6_network" :string, "primary_disk_size" :number, "rebuild_protection" :bool, "name" :string, "network" [:set [:object {"alias_ips" [:set :string], "ip" :string, "mac_address" :string, "network_id" :number}]], "labels" [:map :string], "location" :string, "status" :string, "ipv4_address" :string, "datacenter" :string, "rescue" :string}]] [:map :string] [:list [:object {"id" :number, "delete_protection" :bool, "server_id" :number, "name" :string, "ip_network" :string, "labels" [:map :string], "type" :string, "ip_address" :string, "description" :string, "home_location" :string}]] :number [:set :string] [:list [:object {"rapid_deploy" :bool, "created" :string, "os_flavor" :string, "architecture" :string, "id" :number, "name" :string, "labels" [:map :string], "deprecated" :string, "type" :string, "selector" :string, "os_version" :string, "description" :string}]] [:list [:object {"apply_to" [:set [:object {"label_selector" :string, "server" :number}]], "id" :number, "labels" [:map :string], "name" :string, "rule" [:set [:object {"description" :string, "destination_ips" [:set :string], "direction" :string, "port" :string, "protocol" :string, "source_ips" [:set :string]}]]}]] [:list [:object {"ipv4" :string, "network_zone" :string, "ipv6" :string, "id" :number, "delete_protection" :bool, "algorithm" [:list [:object {"type" :string}]], "name" :string, "labels" [:map :string], "location" :string, "network_id" :number, "target" [:list [:object {"label_selector" :string, "server_id" :number, "type" :string}]], "service" [:list [:object {"destination_port" :number, "health_check" [:list [:object {"http" [:list [:object {"domain" :string, "path" :string, "response" :string, "status_codes" [:list :number], "tls" :bool}]], "interval" :number, "port" :number, "protocol" :string, "retries" :number, "timeout" :number}]], "http" [:list [:object {"certificates" [:list :string], "cookie_lifetime" :number, "cookie_name" :string, "redirect_http" :bool, "sticky_sessions" :bool}]], "listen_port" :number, "protocol" :string, "proxyprotocol" :bool}]], "load_balancer_type" :string, "network_ip" :string}]] [:set :number] [:list [:object {"id" :number, "labels" [:map :string], "name" :string, "servers" [:list :number], "type" :string}]] [:list [:object {"label_selector" :string, "server_id" :number, "type" :string}]] :string [:list [:object {"assignee_type" :string, "id" :number, "delete_protection" :bool, "name" :string, "ip_network" :string, "labels" [:map :string], "location" :string, "assignee_id" :number, "type" :string, "ip_address" :string, "datacenter" :string, "auto_delete" :bool}]] [:list :number] :nil [:list :string] [:list [:object {"delete_protection" :bool, "expose_routes_to_vswitch" :bool, "id" :number, "ip_range" :string, "labels" [:map :string], "name" :string}]] [:list [:object {"delete_protection" :bool, "id" :number, "labels" [:map :string], "linux_device" :string, "location" :string, "name" :string, "server_id" :number, "size" :number}]] :bool [:list [:object {"domain_names" [:list :string], "created" :string, "fingerprint" :string, "not_valid_before" :string, "id" :number, "certificate" :string, "name" :string, "labels" [:map :string], "type" :string, "not_valid_after" :string}]] [:list [:object {"type" :string}]] [:list [:object {"destination_port" :number, "health_check" [:list [:object {"http" [:list [:object {"domain" :string, "path" :string, "response" :string, "status_codes" [:list :number], "tls" :bool}]], "interval" :number, "port" :number, "protocol" :string, "retries" :number, "timeout" :number}]], "http" [:list [:object {"certificates" [:list :string], "cookie_lifetime" :number, "cookie_name" :string, "redirect_http" :bool, "sticky_sessions" :bool}]], "listen_port" :number, "protocol" :string, "proxyprotocol" :bool}]]}]
-    (-> xs
-        (->> (map #(identity {:type %})))
-        first
-        (->> (pr/clj-map->proto-map provider-mapper Schema$Attribute)))))
-
 (comment
   (def foo *1)
-  (pr/clj-map->proto-map provider-mapper GetProviderSchema$Response {:provider {:block {:attributes [{:type :string}]}}}))
+  (->> foo
+       (s/select [s/ALL s/FIRST])
+       #_(s/select [s/ALL #(= :planResourceChange (first %))])))
